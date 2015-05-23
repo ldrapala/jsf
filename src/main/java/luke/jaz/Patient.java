bee9ed3c68bf975ca97ef9b865ac59d308949a0e
Lukasz <Lukasz@luke>
2015-05-23 21:11:37 +0200

    punkt 1 i 2

---
+++ src/main/java/luke/jaz/Patient.java
@@ -0,0 +1,89 @@
+package luke.jaz;
+
+import java.util.Date;
+
+public class Patient {
+
+    private String dateAdded;
+    private String name;
+    private String surname;
+    private String pesel;
+    private Date birthDate;
+    private String address;
+    private int phoneNumber;
+    private float weight;
+    private float height; 
+    
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }  
+    
+}
---
+++ src/main/java/luke/jaz/validator/PatientBean.java
@@ -0,0 +1,66 @@
+package luke.jaz.validator;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.model.ListDataModel;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+import javax.inject.Inject;
+import luke.jaz.Patient;
+import luke.jaz.repository.PatientRepository;
+
+@SessionScoped
+@Named("patientBean")
+public class PatientBean implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Patient patient = new Patient();
+    
+    private ListDataModel<Patient> patientData = new ListDataModel<Patient>();
+
+    public PatientBean() {
+        this.db = new PatientRepository();
+    }
+        
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    
+    public ListDataModel<Patient> getPatientData(){
+        patientData.setWrappedData(db.getList());
+        return patientData;
+    }
+    
+    @Inject
+    PatientRepository db;
+    
+    public String addNewPatient(){
+        db.addPatientToList(patient);              
+        return "listPatient";
+    }
+    
+    public void uniquePesel(FacesContext context, UIComponent component, Object value) {       
+        String pesel = (String)value;
+        System.out.println(pesel);
+        
+        for (Patient patient : db.getList()) {           
+            if (patient.getPesel().equalsIgnoreCase(pesel)) {
+		throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Nie można dublować numerów PESEL.", null));
+            }
+        }        
+    }
+    
+}
+        
+    
+
+
+
